edc01ebc7c3bd69ced8ca384e43ee4dd10915215
sakai shunsuke <sakai@ans-web.co.jp>
2013-05-09 12:51:05 +0900

    設定項目の追加

--- lib/ans-labelizer.rb
+++ lib/ans-labelizer.rb
@@ -5,13 +5,19 @@ module Ans
     class Config
       def initialize
         @locale_path = "activerecord.flags"
+        @hash_method_suffix = "_labels"
+        @inverse_method_suffix = "_keys"
+        @label_method_suffix = "_label"
       end
 
-      attr_accessor :locale_path
+      attr_accessor :locale_path, :hash_method_suffix, :inverse_method_suffix, :label_method_suffix
     end
 
     def self.config
       @config ||= Config.new
+    end
+    def self.configure
+      yield config
     end
 
     def self.included(m)
@@ -20,19 +26,24 @@ module Ans
 
       config = self.config
 
-      ::I18n.t("#{config.locale_path}.#{m.model_name.underscore}", default: {}).each do |column,hash|
+      locale_path = config.locale_path
+      hash_method_suffix = config.hash_method_suffix
+      inverse_method_suffix = config.inverse_method_suffix
+      label_method_suffix = config.label_method_suffix
+
+      ::I18n.t("#{locale_path}.#{m.model_name.underscore}", default: {}).each do |column,hash|
         inverse = hash.invert
 
         ClassMethods.class_eval do
-          define_method :"#{column}_labels" do
+          define_method :"#{column}#{hash_method_suffix}" do
             hash
           end
-          define_method :"#{column}_keys" do
+          define_method :"#{column}#{inverse_method_suffix}" do
             inverse
           end
         end
         InstanceMethods.class_eval do
-          define_method :"#{column}_label" do
+          define_method :"#{column}#{label_method_suffix}" do
             hash[send :"#{column}"]
           end
         end
