7c20c3a74956ed7823a74713793ae8343cdb22ab
sakai shunsuke <sakai@ans-web.co.jp>
2013-05-09 12:43:19 +0900

    ラベルを追加するメソッドの追加

--- lib/ans-labelizer.rb
+++ lib/ans-labelizer.rb
@@ -2,6 +2,46 @@ require "ans-labelizer/version"
 
 module Ans
   module Labelizer
-    # Your code goes here...
+    class Config
+      def initialize
+        @locale_path = "activerecord.flags"
+      end
+
+      attr_accessor :locale_path
+    end
+
+    def self.config
+      @config ||= Config.new
+    end
+
+    def self.included(m)
+      m.send :include, InstanceMethods
+      m.send :extend, ClassMethods
+
+      config = self.config
+
+      ::I18n.t("#{config.locale_path}.#{m.model_name.underscore}", default: {}).each do |column,hash|
+        inverse = hash.invert
+
+        ClassMethods.class_eval do
+          define_method :"#{column}_labels" do
+            hash
+          end
+          define_method :"#{column}_keys" do
+            inverse
+          end
+        end
+        InstanceMethods.class_eval do
+          define_method :"#{column}_label" do
+            hash[send :"#{column}"]
+          end
+        end
+      end
+    end
+
+    module InstanceMethods
+    end
+    module ClassMethods
+    end
   end
 end
